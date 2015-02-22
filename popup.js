78469b88831d21a980a6195dad81bf7ae01af597
Dylan <chillindylan101@gmail.com>
2015-02-22 04:35:27 -0500

    window location
    windows now go into corners! VERY NICE DILI D

--- popup.js
+++ popup.js
@@ -1,14 +1,37 @@
- window.onload = function() {    
+ window.onload = function() {
+    var a = "quasimoonlightlover";
+
+
+    
     function myFunction()
     {
         a=document.getElementById("person").value;
+        a.split();
+        var url = "";
+        var i;
+        for (i = 0; i < a.length; i++)
+        {
+            url+=a[i];
+        }
 
+       // var strWindowFeatures = "location=yes,height=400,width=640,scrollbars=yes,status=yes";
         
+        var sites = [" facebook", " twitter", " tumblr", " instagram"];
 
-        var strWindowFeatures = "location=yes,height=570,width=520,scrollbars=yes,status=yes";
-        var URL = "http://www.google.com/search?q=+"+a+"+&btnI";
-        var win = window.open(URL, "_blank", strWindowFeatures);
+        for(i = 0; i < 4; i++ )
+    {
+        var URL = "http://www.google.com/search?q=+"+url+sites[i]+"+&btnI";
+        if(i ==0 )
+        var win = window.open(URL, "_blank", "toolbar=yes, scrollbars=yes, resizable=yes, top=0, left=0, width=640, height=375");
+            if(i ==1 )
+        var win = window.open(URL, "_blank", "toolbar=yes, scrollbars=yes, resizable=yes, top=450, left=0, width=640, height=375");
+     if(i ==2 )
+        var win = window.open(URL, "_blank", "toolbar=yes, scrollbars=yes, resizable=yes, top=450, left=640, width=640, height=375");
+     if(i ==3 )
+        var win = window.open(URL, "_blank", "toolbar=yes, scrollbars=yes, resizable=yes, top=0, left=640, width=640, height=375");
+    }
+
+
     }
     document.getElementById('thing').onclick = myFunction;
 }
-        document.getElementById('demo').textContent=a;
