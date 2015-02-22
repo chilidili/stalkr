fd235630c7f7f63870d6005d66c505075886e07b
Dylan <chillindylan101@gmail.com>
2015-02-22 07:02:17 -0500

    icon and idk
    fisi

--- popup.js
+++ popup.js
@@ -1,37 +1,50 @@
  window.onload = function() {
-    var a = "quasimoonlightlover";
 
-
-    
     function myFunction()
     {
         a=document.getElementById("person").value;
+        
         a.split();
+        
         var url = "";
+        
         var i;
+        
         for (i = 0; i < a.length; i++)
         {
             url+=a[i];
         }
-
-       // var strWindowFeatures = "location=yes,height=400,width=640,scrollbars=yes,status=yes";
         
         var sites = [" facebook", " twitter", " tumblr", " instagram"];
 
-        for(i = 0; i < 4; i++ )
+
+        for(i = 0; i < 5; i++ )
     {
         var URL = "http://www.google.com/search?q=+"+url+sites[i]+"+&btnI";
-        if(i ==0 )
-        var win = window.open(URL, "_blank", "toolbar=yes, scrollbars=yes, resizable=yes, top=0, left=0, width=640, height=375");
-            if(i ==1 )
-        var win = window.open(URL, "_blank", "toolbar=yes, scrollbars=yes, resizable=yes, top=450, left=0, width=640, height=375");
-     if(i ==2 )
-        var win = window.open(URL, "_blank", "toolbar=yes, scrollbars=yes, resizable=yes, top=450, left=640, width=640, height=375");
-     if(i ==3 )
-        var win = window.open(URL, "_blank", "toolbar=yes, scrollbars=yes, resizable=yes, top=0, left=640, width=640, height=375");
-    }
+        var URLimg = "https://www.google.ca/search?q="+url+sites[i]+"+wallpaper&source=lnms&tbm=isch&sa=X&ei"
+        
+        if(i == 0)
+            var win = window.open(URLimg, "_blank", "toolbar=no, scrollbars=yes, resizable=yes, top=0, left=0, width=1280, height=735");
+
+        if(i ==1 )
+            var win = window.open(URL, "_blank", "toolbar=no, scrollbars=yes, resizable=yes, top=0, left=0, width=640, height=365");
+       
+        if(i ==2 )
+            var win = window.open(URL, "_blank", "toolbar=no, scrollbars=yes, resizable=yes, top=450, left=0, width=640, height=365");
+        
+        if(i ==3 )
+            var win = window.open(URL, "_blank", "toolbar=no, scrollbars=yes, resizable=yes, top=450, left=640, width=640, height=365");
+       
+        if(i ==4 )
+            var win = window.open(URL, "_blank", "toolbar=no, scrollbars=yes, resizable=yes, top=0, left=640, width=640, height=365");
 
 
+
+      
+
     }
+
+    }
+
     document.getElementById('thing').onclick = myFunction;
 }
